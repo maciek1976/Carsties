64e8fc22ab66aad3f956eb0ee1e94b37c913ff7c
maciek1976 <obluzeok@yahoo.com>
2024-10-24 22:41:00 +0200

    End of section 2

---
+++ src/AuctionService/Data/Migrations/20241024162939_ChangeAuctionEndPropertyName.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace AuctionService.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class ChangeAuctionEndPropertyName : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "AuttionEnd",
+                table: "Auctions",
+                newName: "AuctionEnd");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "AuctionEnd",
+                table: "Auctions",
+                newName: "AuttionEnd");
+        }
+    }
+}
