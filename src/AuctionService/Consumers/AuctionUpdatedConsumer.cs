fa6c057589fc2166438819027883f75c27aa9e8e
maciek1976 <obluzeok@yahoo.com>
2024-10-31 00:33:20 +0100

    End of section 6

---
+++ src/AuctionService/Consumers/AuctionUpdatedConsumer.cs
@@ -0,0 +1,23 @@
+﻿using AuctionService.Data;
+using Contracts;
+using MassTransit;
+
+namespace AuctionService.Consumers
+{
+    public class AuctionUpdatedConsumer : IConsumer<AuctionUpdated>
+    {
+        private readonly AuctionDbContext _dbContext;
+
+        public AuctionUpdatedConsumer(AuctionDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public async Task Consume(ConsumeContext<AuctionUpdated> context)
+        {
+            Console.WriteLine("--> Consuming auction updated");
+
+            var auction = await _dbContext.Auctions.FindAsync(context.Message.Id);
+        }
+    }
+}
