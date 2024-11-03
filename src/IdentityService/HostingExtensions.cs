1c3654a3f5931613e70f31a8d88c115ca928857e
maciek1976 <obluzeok@yahoo.com>
2024-11-03 22:22:03 +0100

    End of section 8

--- src/IdentityService/HostingExtensions.cs
+++ src/IdentityService/HostingExtensions.cs
@@ -31,7 +31,7 @@ internal static class HostingExtensions
 
                 if (builder.Environment.IsEnvironment("Docker"))
                 {
-                    options.IssuerUri = "http://localhost:5000";
+                    options.IssuerUri = "http://identity-svc";
                 }
 
                 // see https://docs.duendesoftware.com/identityserver/v6/fundamentals/resources/
--- src/IdentityService/Pages/Diagnostics/Index.cshtml.cs
+++ src/IdentityService/Pages/Diagnostics/Index.cshtml.cs
@@ -16,16 +16,16 @@ public class Index : PageModel
 
     public async Task<IActionResult> OnGet()
     {
-        var localAddresses = new List<string?> { "127.0.0.1", "::1" };
-        // if(HttpContext.Connection.LocalIpAddress != null)
-        // {
-        //     localAddresses.Add(HttpContext.Connection.LocalIpAddress.ToString());
-        // }
+        var localAddresses = new List<string?> {"::ffff:172.18.0.1", "127.0.0.1", "::1",  HttpContext.Connection.LocalIpAddress.ToString()};
+        if(HttpContext.Connection.LocalIpAddress != null)
+        {
+            localAddresses.Add(HttpContext.Connection.LocalIpAddress.ToString());
+        }
 
-        // if (!localAddresses.Contains(HttpContext.Connection.RemoteIpAddress?.ToString()))
-        // {
-        //     return NotFound();
-        // }
+        if (!localAddresses.Contains(HttpContext.Connection.RemoteIpAddress?.ToString()))
+        {
+            return NotFound();
+        }
 
         View = new ViewModel(await HttpContext.AuthenticateAsync());
             
